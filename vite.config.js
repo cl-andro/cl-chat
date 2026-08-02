0ad1ab369c3b760bc18b5a41d0d0f55b8f480a70
ZaidAlamgir <zaidkhan137782@gmail.com>
2026-08-02 22:19:32 +0530

    build: Bundle app into single file using vite-plugin-singlefile

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [svelte()],
-  base: process.env.BASE_URL || '/',
+  plugins: [svelte(), viteSingleFile()],
+  base: './',
 })
